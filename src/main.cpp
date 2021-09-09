c8579227cc2a577c81e7e2280f4410b3688c0c22
rudsomlima <rudsomlima@gmail.com>
2021-09-09 18:24:40 -0300

    Timer ok

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,19 @@
 #include <Adafruit_GFX.h>
 #include <Fonts/FreeSans12pt7b.h>
 #include <TimeLib.h>
+#include <PID_v1.h>
 
 ///////// VARIÁVEIS
 int32 cron, timer;
 int h, m, s;
+float pwm;
+int saida = 1;
+
+//Define Variables we'll be connecting to
+double Setpoint, Input, Output;
+
+//Specify the links and initial tuning parameters
+PID myPID(&Input, &Output, &Setpoint, 100, 10, 10, DIRECT);
 
 ///////// DEFINIÇÃO DO DISPLAY
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
@@ -31,13 +40,19 @@ float read_tm75()
   // In 12-bit mode, resolution is 0.0625°C (1/16th)
   return tm75_reg / float (16);
 }
-extern volatile unsigned long timer0_millis; //Pra poder zerar o timer 0 lá na frente 
+extern volatile unsigned long timer0_millis; //Pra poder zerar o timer 0 lá na frente
+
+///////// DEFINIÇÃO DO PID
+// #define POT_PIN A0
+#define OUTPUT_PIN 2
+
 
 void setup()
 {
   delay(100);
   pinMode(4, INPUT_PULLUP);
   pinMode(5, INPUT_PULLUP);
+  pinMode(OUTPUT_PIN, OUTPUT);
   //digitalWrite(led, HIGH);
   Wire.begin(I2C_SDA, I2C_SCL);
   Serial.begin(9600);
@@ -70,29 +85,47 @@ void setup()
   // TIMER ------------------------------------------------------
   time_t t = now();
   setTime(0, 0, 0, 4, 9, 2021);
+  h = 1;
+  m = 59;
+  s = 59;
+
+  // PID ------------------------------------------------------
+  Setpoint = 30.0;
+
+  //turn the PID on
+  myPID.SetMode(AUTOMATIC);
 }
 
 void loop()
 {
+  Input = read_tm75();
   display.clearDisplay();
-  float temp = read_tm75();
+  // float temp = read_tm75();
   // display.setFont(&FreeSans12pt7b);
   display.setTextSize(2);
   display.setCursor(0, 0);
-  display.println("TEMP/TIMER");
-  // display.setTextSize(2);
-  display.setTextColor(WHITE);
-  // display.setCursor(0, 33);
-  display.printf("%4.1f", temp);
-  // display.drawCircle(55, 18, 2, WHITE);
-  // display.setCursor(0, 13);
-  display.println(" C");
-  // display.setCursor(0, 30);
-  display.printf("%uh%um%us",1-hour(),59-minute(),59-second());
+  display.printf("ATUAL:%4.1f", Input);
+  display.printf("SET:  %4.1f", Setpoint);
+  display.printf("PWM: %5.1f", Output);
+  uint hora = h - hour();
+  uint minuto = m - minute();
+  uint segundo = s - second();
+  if(hora==0 & minuto==0 & segundo==0)
+  {
+    Output = 0;
+    analogWrite(OUTPUT_PIN, Output);  // desliga saida
+    saida = 0;
+  }
+  if(saida==0) display.println("DESLIGADO");
+  else
+  {
+    myPID.Compute();
+    display.printf("%02dh%02dm%02ds", hora, minuto, segundo);
+    Serial.print(Input);
+    Serial.print(" ");
+    Serial.print(second());
+    Serial.print(" ");
+    Serial.println(Output);
+  }
   display.display();
-  Serial.print(temp);
-  Serial.print(" ");
-  Serial.print(second());
-  Serial.println();
-  delay(1000);
 }
